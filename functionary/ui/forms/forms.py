d6cfd236c3665e29186f35460cc5ae8e60ad315f
kvig <kvig@geontech.com>
2022-09-20 17:00:04 +0000

    Add additional parameter types.
    Updated the supported schema parameter types, making sure to save the
    additional type information needed for correct display while ensuring
    the parsing of the schema.
    Added additional form fields for the extra types.
    Added rerender of the function page when validation fails.

--- functionary/ui/forms/forms.py
+++ functionary/ui/forms/forms.py
@@ -1,27 +1,52 @@
+import json
+
 from django.forms import (
     BooleanField,
     CharField,
     DateField,
     DateTimeField,
-    DecimalField,
     FloatField,
     Form,
     IntegerField,
     JSONField,
-    TimeField,
+    Textarea,
 )
 
 _field_mapping = {
-    "integer": IntegerField,
-    "string": CharField,
-    "float": FloatField,
-    "double": DecimalField,
-    "boolean": BooleanField,
-    "date": DateField,  # TODO set input_formats= on constructor if needed
-    "datetime": DateTimeField,  # TODO set input_formats= on constructor if needed
-    "time": TimeField,  # TODO set input_formats= on constructor if needed
-    "object": JSONField,
+    "integer": (IntegerField, None),
+    "string": (CharField, None),
+    "text": (CharField, Textarea),
+    "number": (FloatField, None),
+    "boolean": (BooleanField, None),
+    "date": (DateField, None),
+    "date-time": (
+        DateTimeField,
+        None,
+    ),
+    "json": (JSONField, Textarea),
 }
+
+_transform_intitial_mapping = {"json": json.loads}
+
+
+def get_param_type(param_dict):
+    keys = param_dict.keys()
+
+    # pydantic hides the date type in the format field
+    if "format" in keys:
+        return param_dict["format"]
+    # json and text get mapped to TypeVars to preserve the distinction vs string
+    elif "anyOf" in keys:
+        return (
+            "json"
+            if any(
+                param_types.get("format", None) == "json-string"
+                for param_types in param_dict["anyOf"]
+            )
+            else "text"
+        )
+    else:
+        return param_dict["type"]
 
 
 class TaskParameterForm(Form):
@@ -31,20 +56,28 @@ class TaskParameterForm(Form):
         super().__init__(data)
 
         for param, value in function.schema["properties"].items():
-            req: bool = not value["default"]
+            req = not value["default"]
             initial = value.get("default", None)
+            param_type = get_param_type(value)
             kwargs = {
                 "label": value["title"],
-                "label_suffix": value["type"],
+                "label_suffix": param_type,
                 "initial": initial,
                 "required": req,
                 "help_text": value.get("description", None),
             }
-            field_class = _field_mapping.get(value["type"], None)
+
+            field_class, widget = _field_mapping.get(param_type, (None, None))
 
             if not field_class:
-                raise ValueError(f"Unknown field type for {param}: {value['type']}")
+                raise ValueError(f"Unknown field type for {param}: {param_type}")
+
+            if widget:
+                kwargs["widget"] = widget
+            if param_type in _transform_intitial_mapping:
+                kwargs["initial"] = _transform_intitial_mapping[param_type](initial)
 
             field = field_class(**kwargs)
-            field.widget.attrs.update({"class": "input"})
+            if param_type != "boolean":
+                field.widget.attrs.update({"class": "input"})
             self.fields[param] = field
