66ac489bf9774342dc7f8c0a52f20a907a0d488d
kvig <kvig@geontech.com>
2022-09-22 11:41:59 +0000

    Fixing code review comments. Fix task details result display

--- functionary/ui/forms/forms.py
+++ functionary/ui/forms/forms.py
@@ -36,10 +36,18 @@ _field_mapping = {
     "json": (JSONField, Textarea),
 }
 
-_transform_intitial_mapping = {"json": json.loads}
+_transform_initial_mapping = {"json": json.loads}
 
 
-def get_param_type(param_dict):
+def _get_param_type(param_dict):
+    """Finds the type of the parameter from the definition in the schema.
+
+    Pydantic maps to python types correctly, but we want to use the actual
+    schema type for input purposes. If there is a format in the param_dict,
+    the input field has a definition of what type it is. If there is an anyOf,
+    inspect it and try find out what type it should be. Otherwise, return the
+    value of 'type' in param_dict.
+    """
     keys = param_dict.keys()
 
     # pydantic hides the date type in the format field
@@ -59,6 +67,22 @@ def get_param_type(param_dict):
         return param_dict["type"]
 
 
+def _prepare_initial_value(param_type, initial):
+    """Convert the initial value to the appropriate type.
+
+    This function will massage the initial value as needed into the type
+    required for the parameter field. Currently, JSON types need to be
+    converted from a string into an object, otherwise display issues
+    occur in the form.
+    """
+    if initial:
+        if param_type in _transform_initial_mapping:
+            return _transform_initial_mapping[param_type](initial)
+        else:
+            return initial
+    return None
+
+
 class TaskParameterForm(Form):
     template_name = "forms/task_parameters.html"
 
@@ -66,28 +90,28 @@ class TaskParameterForm(Form):
         super().__init__(data)
 
         for param, value in function.schema["properties"].items():
-            req = not value["default"]
             initial = value.get("default", None)
-            param_type = get_param_type(value)
-            kwargs = {
-                "label": value["title"],
-                "label_suffix": param_type,
-                "initial": initial,
-                "required": req,
-                "help_text": value.get("description", None),
-            }
-
+            req = initial is None
+            param_type = _get_param_type(value)
             field_class, widget = _field_mapping.get(param_type, (None, None))
 
             if not field_class:
                 raise ValueError(f"Unknown field type for {param}: {param_type}")
 
+            kwargs = {
+                "label": value["title"],
+                "label_suffix": param_type,
+                "initial": _prepare_initial_value(param_type, initial),
+                "required": req,
+                "help_text": value.get("description", None),
+            }
+
             if widget:
                 kwargs["widget"] = widget
-            if param_type in _transform_intitial_mapping:
-                kwargs["initial"] = _transform_intitial_mapping[param_type](initial)
 
             field = field_class(**kwargs)
+
+            # Style all inputfields except the checkbox with the "input" class
             if param_type != "boolean":
                 field.widget.attrs.update({"class": "input"})
             self.fields[param] = field
