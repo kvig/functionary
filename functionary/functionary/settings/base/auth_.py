6411901d658f03dfc0ea5b64771feb12f342f1e2
kvig <kvig@geontech.com>
2022-12-19 19:30:38 +0000

    Refs #152 - Add third party auth support
    Integrates django-allauth for supporting oauth2 and openid.
    Additional oauth services are able to be configured for authentication
    in addition to the standard login/password.

---
+++ functionary/functionary/settings/base/auth_.py
@@ -0,0 +1,14 @@
+SITE_ID = 1
+
+ACCOUNT_AUTHENTICATION_METHOD = "username"
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 900
+ACCOUNT_PRESERVE_USERNAME_CASING = False
+ACCOUNT_SESSION_REMEMBER = True
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SOCIALACCOUNT_PROVIDERS = {}
