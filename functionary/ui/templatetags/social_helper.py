6ea913993be900d850bde3d987cef0c0cf609abf
kvig <kvig@geontech.com>
2022-12-22 19:30:10 +0000

    Addressing review comments
    Fix the third party login prompt.

--- functionary/ui/templatetags/social_helper.py
+++ functionary/ui/templatetags/social_helper.py
@@ -1,10 +1,21 @@
+from allauth.socialaccount.providers.base import ProviderAccount
 from django import template
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def find_account(context, provider_id):
+def find_account(context, provider_id: str) -> ProviderAccount | None:
+    """This tag checks the SocialAccounts in the form found in the
+    templates context and returns the desired ProviderAccount.
+
+    Args:
+        context: the template's context, automatically passed in
+        provider_id: ID of the SocialAccount to find
+
+    Returns:
+        The matching ProviderAccount or None
+    """
     if "form" in context:
         for account in context["form"].accounts:
             if account.provider == provider_id:
