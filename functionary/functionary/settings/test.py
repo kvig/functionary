1ef0fd06b62d06bf8cfb6ebb5c081006ed4da359
kvig <kvig@geontech.com>
2023-02-02 16:53:47 +0000

    Add NAME setting DATABASES in test.py

--- functionary/functionary/settings/test.py
+++ functionary/functionary/settings/test.py
@@ -1,5 +1,5 @@
 from .base import *  # noqa
 
 CELERY_BROKER_URL = "memory://"
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "testdb"}}
 SECRET_KEY = "testsecret"
